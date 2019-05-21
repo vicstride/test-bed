a3296b528d54b212a5e217ae53fe696a8e9ef93c
veng <veng@2u.com>
2019-05-21 14:17:39 -0400

    Add some quick hacky css fixes in lieu of normalizing

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ import Test from './components/Test';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello, world!
-      </header>
       <Test />
     </div>
   );
